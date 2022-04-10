20ae005a1f12433c14683d53957d9b41a91100b8
Leonardo Nakahara <leonardo.nakahara@gmail.com>
2022-04-10 19:05:09 -0300

    Initial Commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x029b0537a4950f1D93B11BE4684Bdb78b545cea8";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
